b7d2a1e22a9c74d2247875c5cc490426ecf3835a
Gustavo de Assis <gustavo.siqueira.421@gmail.com>
2022-09-17 13:13:52 -0300

    Ajuste com props: Sugestoes

--- src/Sugestoes.js
+++ src/Sugestoes.js
@@ -1,70 +1,36 @@
-export default function Sugestoes(){
-    return(
+function Sugestao(props) {
+    return (
+        <div class="sugestao">
+            <div class="usuario">
+                <img src={props.imagem} alt={props.nome}/>
+                <div class="texto">
+                    <div class="nome">{props.nome}</div>
+                    <div class="razao">{props.razao}</div>
+                </div>
+            </div>
+
+            <div class="seguir">Seguir</div>
+        </div>
+    )
+}
+
+
+export default function Sugestoes() {
+    const sugestoes=[
+        {nome:'bad.vibes.memes',imagem:"assets/img/bad.vibes.memes.svg", razao:"Segue Você" },
+        {nome:'chibirdart',imagem:"assets/img/chibirdart.svg", razao:"Segue Você"},
+        {nome:'razoesparaacreditar' ,imagem:"assets/img/razoesparaacreditar.svg", razao:"Novo no Instagram" },
+        {nome:'adorable_animals' ,imagem:"assets/img/adorable_animals.svg" ,razao:"Segue Você"},
+        {nome:'smallcutecats' ,imagem:"assets/img/smallcutecats.svg" ,razao:"Segue Você"}
+    ];
+    
+    return (
         <div class="sugestoes">
             <div class="titulo">
                 Sugestões para você
                 <div>Ver tudo</div>
             </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/bad.vibes.memes.svg" />
-                    <div class="texto">
-                        <div class="nome">bad.vibes.memes</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/chibirdart.svg" />
-                    <div class="texto">
-                        <div class="nome">chibirdart</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/razoesparaacreditar.svg" />
-                    <div class="texto">
-                        <div class="nome">razoesparaacreditar</div>
-                        <div class="razao">Novo no Instagram</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/adorable_animals.svg" />
-                    <div class="texto">
-                        <div class="nome">adorable_animals</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                    <img src="assets/img/smallcutecats.svg" />
-                    <div class="texto">
-                        <div class="nome">smallcutecats</div>
-                        <div class="razao">Segue você</div>
-                    </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
+            {sugestoes.map((s,i)=>(<Sugestao nome={s.nome} imagem={s.imagem} razao={s.razao} key={i}/>))}
         </div>
     )
 }
