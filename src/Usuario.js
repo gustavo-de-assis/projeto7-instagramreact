9b47716418640de59af1b5dbd0d6fc6d9570265b
Gustavo de Assis <gustavo.siqueira.421@gmail.com>
2022-09-17 13:24:06 -0300

    Arrays: Dados passados como arrays.map e Usuario com props

--- src/Usuario.js
+++ src/Usuario.js
@@ -1,14 +1,22 @@
-export default function Usuario(){
-    return (
-        <div class="usuario">
-          <img src="assets/img/catanacomics.svg" />
-          <div class="texto">
-            <strong>catanacomics</strong>
-            <span>
-              Catana
-              <ion-icon name="pencil"></ion-icon>
-            </span>
-          </div>
-        </div>
-    );
+function InformacoesUsuario(props) {
+  return (
+    <>
+      <img src={props.imagem} alt={props.nome}/>
+      <div class="texto">
+        <strong>{props.nickname}</strong>
+        <span>
+          {props.nome}
+          <ion-icon name="pencil"></ion-icon>
+        </span>
+      </div>
+    </>
+  )
+}
+
+export default function Usuario() {
+  return (
+    <div class="usuario">
+      <InformacoesUsuario imagem={"assets/img/catanacomics.svg"} nome={'Catana'} nickname={'catanacomics'}/>
+    </div>
+  );
 }
