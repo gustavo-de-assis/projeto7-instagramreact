b73f6fc2e648922f440e53e023bba59ea17408bb
Gustavo de Assis <gustavo.siqueira.421@gmail.com>
2022-09-17 13:42:02 -0300

    Dados dinâmicos: infos do usuário

--- src/Usuario.js
+++ src/Usuario.js
@@ -1,12 +1,14 @@
+import React, {useState} from "react";
+
 function InformacoesUsuario(props) {
   return (
     <>
-      <img src={props.imagem} alt={props.nome}/>
+      <img src={props.imagem} alt={props.nome} onClick={props.funcaoImagem}/>
       <div class="texto">
         <strong>{props.nickname}</strong>
         <span>
           {props.nome}
-          <ion-icon name="pencil"></ion-icon>
+          <ion-icon name="pencil" onClick={props.funcaoNome}></ion-icon>
         </span>
       </div>
     </>
@@ -14,9 +16,24 @@ function InformacoesUsuario(props) {
 }
 
 export default function Usuario() {
+  const [nomeUsuario, setNome] = useState('Catana');
+  const [imagemUsuario, setImagem] = useState("assets/img/catanacomics.svg");
+  
+  function mudaNomeUsuario(){
+    const novoNome = prompt("Entre com um novo nome:");
+    setNome(novoNome);
+  }
+
+  function mudaImagemUsuario(){
+    const novaImagem = prompt("Entre com um link da Imagem:");
+    setImagem(novaImagem);
+  }
+
   return (
     <div class="usuario">
-      <InformacoesUsuario imagem={"assets/img/catanacomics.svg"} nome={'Catana'} nickname={'catanacomics'}/>
+      <InformacoesUsuario nome={nomeUsuario} funcaoNome={mudaNomeUsuario}
+      imagem={imagemUsuario} funcaoImagem={mudaImagemUsuario}
+      nickname={'catanacomics'}/>
     </div>
   );
 }
